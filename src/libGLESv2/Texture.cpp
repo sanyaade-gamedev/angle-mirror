sanyaade-gamedev/angle-mirror

commit 79894e03f6c8a507541108db3a143d2f143aee58

Removed an unreachable path in texture loading code
TRAC #21621
Issue=311
Signed-off-by: Shannon Woods
Signed-off-by: Daniel Koch
Author: Nicolas Capens

git-svn-id: http://angleproject.googlecode.com/svn/trunk@1308 736b8ea6-26fd-11df-bfd4-992fa37f6226

src/libGLESv2/Texture.cpp
@@ -297,18 +297,10 @@ void Image::updateSurface(IDirect3DSurface9 *destSurface, GLint xoffset, GLint y
         rect.right = xoffset + width;
         rect.bottom = yoffset + height;
 
-        if (mD3DPool == D3DPOOL_MANAGED)
-        {
-            HRESULT result = D3DXLoadSurfaceFromSurface(destSurface, NULL, &rect, sourceSurface, NULL, &rect, D3DX_FILTER_BOX, 0);
-            ASSERT(SUCCEEDED(result));
-        }
-        else
-        {
-            // UpdateSurface: source must be SYSTEMMEM, dest must be DEFAULT pools 
-            POINT point = {rect.left, rect.top};
-            HRESULT result = getDevice()->UpdateSurface(sourceSurface, &rect, destSurface, &point);
-            ASSERT(SUCCEEDED(result));
-        }
+        // UpdateSurface: source must be SYSTEMMEM, dest must be DEFAULT pools 
+        POINT point = {rect.left, rect.top};
+        HRESULT result = getDevice()->UpdateSurface(sourceSurface, &rect, destSurface, &point);
+        ASSERT(SUCCEEDED(result));
     }
 }
 

commit 170dc5751c114342d6b4383ae6a3b4e7511d8fc2

Fixed support for updating from managed surfaces.
TRAC #21621
Issue=311
Signed-off-by: Daniel Koch
Author: Nicolas Capens
(Fixes XP path that was broken in r1308)

git-svn-id: http://angleproject.googlecode.com/svn/trunk@1313 736b8ea6-26fd-11df-bfd4-992fa37f6226

src/libGLESv2/Texture.cpp
@@ -325,10 +325,30 @@ void Image::updateSurface(IDirect3DSurface9 *destSurface, GLint xoffset, GLint y
         rect.right = xoffset + width;
         rect.bottom = yoffset + height;
 
-        // UpdateSurface: source must be SYSTEMMEM, dest must be DEFAULT pools 
         POINT point = {rect.left, rect.top};
-        HRESULT result = getDevice()->UpdateSurface(sourceSurface, &rect, destSurface, &point);
-        ASSERT(SUCCEEDED(result));
+
+        if (mD3DPool == D3DPOOL_MANAGED)
+        {
+            D3DSURFACE_DESC desc;
+            sourceSurface->GetDesc(&desc);
+
+            IDirect3DSurface9 *surf = 0;
+            HRESULT result = getDevice()->CreateOffscreenPlainSurface(desc.Width, desc.Height, desc.Format, D3DPOOL_SYSTEMMEM, &surf, NULL);
+
+            if (SUCCEEDED(result))
+            {
+                CopyLockableSurfaces(surf, sourceSurface);
+                result = getDevice()->UpdateSurface(surf, &rect, destSurface, &point);
+                ASSERT(SUCCEEDED(result));
+                surf->Release();
+            }
+        }
+        else
+        {
+            // UpdateSurface: source must be SYSTEMMEM, dest must be DEFAULT pools
+            HRESULT result = getDevice()->UpdateSurface(sourceSurface, &rect, destSurface, &point);
+            ASSERT(SUCCEEDED(result));
+        }
     }
 }
 
@@ -1197,7 +1217,6 @@ void GenerateMip(IDirect3DSurface9 *destSurface, IDirect3DSurface9 *sourceSurfac
     ASSERT(SUCCEEDED(result));
 
     ASSERT(sourceDesc.Format == destDesc.Format);
-    ASSERT(sourceDesc.Pool == destDesc.Pool);
     ASSERT(sourceDesc.Width == 1 || sourceDesc.Width / 2 == destDesc.Width);
     ASSERT(sourceDesc.Height == 1 || sourceDesc.Height / 2 == destDesc.Height);
 